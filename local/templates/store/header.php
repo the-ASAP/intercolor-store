91ac5b6ca5ad0afdd5380969983087c3d8a6815d
Дмитрий Кущ <dkussh@code-pilots.ru>
2025-05-13 01:12:36 +0300

    storages list components

--- local/templates/store/header.php
+++ local/templates/store/header.php
@@ -1,29 +1,35 @@
 <html lang="ru">
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <?php
-    $APPLICATION->SetAdditionalCSS("/ui-kit/css/main.css");
-    $APPLICATION->ShowHead(false);
-    ?>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<?php
+	$APPLICATION->SetAdditionalCSS("/ui-kit/css/main.css");
+	$APPLICATION->ShowHead(false);
+	?>
 </head>
 
 <body>
-    <div id="panel"><? $APPLICATION->ShowPanel(); ?></div> 
-    <?$APPLICATION->IncludeComponent("bitrix:menu","left_side",Array(
-		"ROOT_MENU_TYPE" => "top", 
-		"MAX_LEVEL" => "1", 
-		"CHILD_MENU_TYPE" => "top", 
-		"USE_EXT" => "Y",
-		"DELAY" => "N",
-		"ALLOW_MULTI_SELECT" => "Y",
-		"MENU_CACHE_TYPE" => "N", 
-		"MENU_CACHE_TIME" => "3600", 
-		"MENU_CACHE_USE_GROUPS" => "Y", 
-		"MENU_CACHE_GET_VARS" => "" 
-	)
-);?>
+	<? $APPLICATION->IncludeComponent(
+		"bitrix:menu",
+		"left_side",
+		array(
+			"ROOT_MENU_TYPE" => "top",
+			"MAX_LEVEL" => "1",
+			"CHILD_MENU_TYPE" => "top",
+			"USE_EXT" => "Y",
+			"DELAY" => "N",
+			"ALLOW_MULTI_SELECT" => "Y",
+			"MENU_CACHE_TYPE" => "N",
+			"MENU_CACHE_TIME" => "3600",
+			"MENU_CACHE_USE_GROUPS" => "Y",
+			"MENU_CACHE_GET_VARS" => ""
+		)
+	); ?>
 
-    <div class="content-wrapper">
-        <main>
+	<div class="content-wrapper">
+		<main>
+			<? global $USER;
+			if ($USER->IsAdmin()): ?>
+				<div id="panel"><? $APPLICATION->ShowPanel(); ?></div>
+			<? endif; ?>
