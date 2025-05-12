b02f7f628a302ae6dc60f04021225463fde906cd
Дмитрий Кущ <dkussh@code-pilots.ru>
2025-05-13 00:07:24 +0300

    starting Layout

--- local/templates/store/footer.php
+++ local/templates/store/footer.php
@@ -1,2 +1,13 @@
+
+<footer class="footer flex items-center justify-between">
+            <span class="footer__rights">© 2025 Магазин ГК «Интерколор»</span>
+            <div class="footer-buttons flex gap-2">
+                <a href="#" class="footer__btn label-1">О компании</a>
+                <a href="#" class="footer__btn label-1">Соглашение</a>
+                <a href="#" class="footer__btn label-1">Контакты</a>
+            </div>
+        </footer>
+</main>
+</div>
 </body>
 </html>
--- local/templates/store/header.php
+++ local/templates/store/header.php
@@ -4,9 +4,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <?php
+    $APPLICATION->SetAdditionalCSS("/ui-kit/css/main.css");
     $APPLICATION->ShowHead(false);
     ?>
 </head>
 
 <body>
-    <div id="panel"><? $APPLICATION->ShowPanel(); ?></div>
+    <div id="panel"><? $APPLICATION->ShowPanel(); ?></div> 
+    <?$APPLICATION->IncludeComponent("bitrix:menu","left_side",Array(
+		"ROOT_MENU_TYPE" => "top", 
+		"MAX_LEVEL" => "1", 
+		"CHILD_MENU_TYPE" => "top", 
+		"USE_EXT" => "Y",
+		"DELAY" => "N",
+		"ALLOW_MULTI_SELECT" => "Y",
+		"MENU_CACHE_TYPE" => "N", 
+		"MENU_CACHE_TIME" => "3600", 
+		"MENU_CACHE_USE_GROUPS" => "Y", 
+		"MENU_CACHE_GET_VARS" => "" 
+	)
+);?>
+
+    <div class="content-wrapper">
+        <main>
